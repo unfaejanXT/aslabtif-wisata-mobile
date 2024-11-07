8617560e7b3024a2459207933776087436f8ebf9
Janwar Ksatria Pamungkas <janwarksatriapamungkas@hotmail.com>
2024-11-07 14:54:40 +0700

    Materi Pertemuan Ke-5

--- lib/materimodulscreens/formscreen.dart
+++ lib/materimodulscreens/formscreen.dart
@@ -8,12 +8,94 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  bool switchValue = false;
+  bool olahraga = false;
+  bool seni = false;
+  String selectedRadio = '';
+  String nama = '';
+  TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
+      appBar: AppBar(
+        title: Text('Input Form'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                labelText: 'Masukkan Nama',
+              ),
+              onChanged: (String value) {
+                setState(() {
+                  nama = value;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Text('Jenis Kelamin'),
+                Radio(
+                  value: 'Laki-Laki',
+                  groupValue: selectedRadio,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedRadio = value.toString();
+                    });
+                  },
+                ),
+                Text('Laki-Laki'),
+                Radio(
+                  value: 'Perempuan',
+                  groupValue: selectedRadio,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedRadio = value.toString();
+                    });
+                  },
+                ),
+                Text('Perempuan')
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Hobi :'),
+            CheckboxListTile(
+                title: Text('Olahraga'),
+                value: olahraga,
+                onChanged: (value) {
+                  setState(() {
+                    olahraga = value!;
+                  });
+                }),
+            CheckboxListTile(
+                title: Text('Seni'),
+                value: seni,
+                onChanged: (value) {
+                  setState(() {
+                    seni = value!;
+                  });
+                }),
+            SizedBox(
+              height: 20,
+            ),
+            SwitchListTile(
+                title: Text('Lulus'),
+                value: switchValue,
+                onChanged: (value) {
+                  setState(() {
+                    switchValue = value;
+                  });
+                })
+          ],
+        ),
       ),
     );
   }
